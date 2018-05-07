ef92670bd2146b2a3c025f4febfbb8fd54096298
WU Shengzhi <328095481@qq.com>
2018-05-06 23:39:45 -0400

    Shengzhi update
    add background music and animated the logo, also refine some animations for Suns

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 var x,y, x1,y1,x2,y2,x3,y3,x4,y4,TargetX,TargetY,a,b;
 
 var sun=[];
+var sun0;
+var p;
 function setup() {
   x=0;
   y=0;
@@ -8,14 +10,18 @@ function setup() {
   b=1;
   createCanvas(500,500);
   // background(0);
-  for(var i=0;i<6;i++){
-    sun[i]=createImg("images/suns/sun.png")
+  for(var i=0;i<10;i++){
+    sun[i]=createImg("images/suns/sun.png");
+     
   }
+//    sun0=createImg("images/suns/sun.png");
+//    p = new Particle(200, 200);
 }
 
 function draw() {
   // createCanvas(windowWidth, windowHeight);
   // background(0);
+    
   
   TargetX= map(mouseX,0,width,10,0);
   TargetY= map(mouseY,0,height,10,0);
@@ -25,10 +31,10 @@ function draw() {
  
   
   randomSeed(9);
-  for(var i=0;i<6;i++){
-    var size=random(150,300)
-    nx=(noise(i+a)-.5)*260;
-    ny=(noise(i+b)-.5)*260;
+  for(var i=0;i<10;i++){
+    var size=random(100,280)
+    nx=(noise(i+a)-.5)*size*1.5;
+    ny=(noise(i+b)-.5)*size*1.5;
    
     sun[i].size(size,size);
     sun[i].position(random(100,windowWidth-100)+x+nx,random(50,windowHeight/2)+y+ny);
@@ -55,6 +61,44 @@ function draw() {
   // rect(200+x4,300+y4,100,100);
   a+=0.002;
   b+=0.002;
+    
+//    p.update();
+//  p.show();
+}
+
+
+function Particle(x, y) {
+  // this.x = x;
+  // this.y = y;
+  this.pos = createVector(x, y);
+  this.vel = createVector(1, 0);
+  this.acc = createVector(0, 0.01);
+  
+  this.update = function() {
+    var mouse = createVector(mouseX, mouseY);
+    // This is ok but long
+    // this.acc.x = mouse.x - this.pos.x;
+    // this.acc.y = mouse.y - this.pos.y;
+  
+    // This is what i want to write but can't
+    // this.acc = mouse - this.pos;
+    
+    // This is the awkward way
+    this.acc = p5.Vector.sub(mouse, this.pos);
+    // this.acc.mult(0.001);
+    this.acc.setMag(0.1);
+    
+    this.pos.add(this.vel);
+    this.vel.add(this.acc);
+    this.vel.limit(5);
+  }
+
+  this.show = function() {
+    // point(mouseX,mouseY);
+//    strokeWeight(4);
+//    stroke(255);
+    sun0.position(this.pos.x, this.pos.y);
+  }
 }
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
